f7ed0d1e34379eb5704993ae2658bd03b4adc598
Leo Neat <leosneat@gmail.com>
2020-01-16 07:34:35 -0800

    actions testing

--- projects/yara/build.sh
+++ projects/yara/build.sh
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 ################################################################################
+echo $PWD
+ls
 
 ./bootstrap.sh
 ./configure --enable-debug --enable-dex --enable-dotnet --without-crypto
