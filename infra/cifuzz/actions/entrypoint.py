86c70fd27ed08c491ea220dad45b38745f50af75
Leo Neat <leosneat@gmail.com>
2020-01-17 08:04:19 -0800

    Testing action

--- infra/cifuzz/actions/entrypoint.py
+++ infra/cifuzz/actions/entrypoint.py
@@ -24,6 +24,11 @@ def main():
   repo_name = os.environ['GITHUB_REPOSITORY'].rsplit('/', 1)[-1]
   commit_sha = os.environ['GITHUB_SHA']
 
+  print('Checking volumes')
+  subprocess.check_call(['docker','volume','ls'])
+  print('Checking workspace dir')
+  subprocess.check_call(['ls', os.environ['GITHUB_WORKSPACE']])
+
   # Build the specified project's fuzzers from the current repo state.
   print('Building fuzzers\nproject: {0}\nrepo name: {1}\ncommit: {2}'.format(
       project_name, repo_name, commit_sha))
